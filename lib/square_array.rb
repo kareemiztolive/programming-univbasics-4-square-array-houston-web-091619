6d613e1e9423c9c2443573aa0136efa752d642d0
Mubarak Kareem <nelsonsmf@gmail.com>
2019-09-14 22:43:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   counter = 0
   array = [1,2,3]
-  new_numbers = []
+  # new_numbers = []
   
   while array[counter] do
  
- return new_numbers = array[counter]**2
+ return  array[counter]**2
  counter += 1
 end
 
